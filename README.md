34e67e9825730e91b8935ebcb7e646cc1165ffed
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-08-01 22:14:23 -0700

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# CHATGAME SCRIPT | BD-CHATGAMES #
+
+# Description #
+bd-chatgames is a script packed together with a variety of different games, currently only has chat games with updates to come in future
+
+# FEATURES #
+- Dice Command to roll 1-3 dice
+- RPS Command to play rock, paper, scissors
+- Shoot Command to play odds and evens
+- Coinflip command to have a coinflip
+
+# DISCORD #
+- https://discord.gg/hya9t8XfH8
+
+# I HOPE YOU ENJOY #
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+client_script 'cl_chatgames.lua'
+server_script 'sv_chatgames.lua'
+
+files {
+	"chatgames_index.html",
+	"chatgames_js.js",
+	"imagez/option_1.png",
+	"imagez/option_2.png",
+	"imagez/option_3.png",
+	"imagez/dice_1.png",
+	"imagez/dice_2.png",
+	"imagez/dice_3.png",
+	"imagez/dice_4.png",
+	"imagez/dice_5.png",
+	"imagez/dice_6.png",
+	"imagez/coinflip_1.png",
+	"imagez/coinflip_2.png",
+	"imagez/shoot_1.png",
+	"imagez/shoot_2.png",
+}
+
+ui_page {
+	'chatgames_index.html',
+}
